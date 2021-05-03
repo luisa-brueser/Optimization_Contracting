9cdfc5ca5ba63b4ebf07aaa1f773529107faf034
Luisa Brüser <luisa.brueser@gmail.com>
2021-05-03 17:40:16 +0200

    Deleted unnecessary test file

--- src/pv_optimizer_contracting/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from pathlib import Path
-input_file_path = Path(__file__).parent / 'data_input_new_model.xlsx'
-
-general_df = pd.read_excel(io=input_file_path, sheet_name='General Data').drop('Abbreviation', axis=1)   
-print(general_df)
